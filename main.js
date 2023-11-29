83da4ecc1c6140dbdbad471246cd3bf8150c5036
AmirHK888 <amirhkarimi888@gmail.com>
2023-11-29 18:37:48 +0330

    Animations were added to loading sections of main

--- main.js
+++ main.js
@@ -5,8 +5,4 @@ import mainView from './views/mainView';
 headerView.render();
 
 mainView.render();
-
-window.addEventListener("scroll", () => {
-  document.querySelector(".intro2Title").style.animation = "moveInBottom 1s ease-out"
-  document.querySelector(".intro2Sub").style.animation = "moveInBottom 1s ease-out"
-})
+mainView.scrollMain()
--- views/mainView.js
+++ views/mainView.js
@@ -6,6 +6,10 @@ class Main extends App {
 
         this.element = document.querySelector("main");
 
+        this.lastKnownScrollPosition = 0;
+        this.ticking = false;
+
+
         this.markup = /*html*/`
         <div class="mainContainer">
           <div class="intro">
@@ -17,17 +21,63 @@ class Main extends App {
 
           <ul class="intro2">
             <li>
-              <div>
+              <div class="intro2Descr">
                <h1 class="intro2Title">Amir H K</h1>
                <h3 class="intro2Sub">Welcome to my blog!</h3>
               </div>
             </li>
             <li>
-              <img src="https://wallpapershome.com/images/pages/pic_h/3954.jpg" />
+              <img class="intro2Image" src="https://wallpapershome.com/images/pages/pic_h/3954.jpg" />
             </li>
           </ul>
+
+          <ul class="intro3">
+          <li>
+          <img class="intro3Image" src="https://wallpapershome.com/images/pages/pic_h/3945.jpg" />
+        </li>
+          <li>
+            <div class="intro3Descr">
+             <h1 class="intro3Title">Amir H K</h1>
+             <h3 class="intro3Sub">Welcome to my blog!</h3>
+            </div>
+          </li>
+        </ul>
         </div>
         `
+
+    }
+
+
+
+    setAnimations(scrollPos) {
+
+        if(scrollPos >= window.innerHeight - 60 && scrollPos <= (2 * window.innerHeight) - 60) {
+          document.querySelectorAll(".intro2 li").forEach(el => el.style.opacity = "1");
+          document.querySelector(".intro2Image").style.animation = "moveInRight 1s"
+          document.querySelector(".intro2Descr").style.animation = "moveInLeft 1s"
+        }
+        
+        if(scrollPos >= (2 * window.innerHeight) - 60 && scrollPos <= (3 * window.innerHeight) - 60) {
+          document.querySelectorAll(".intro3 li").forEach(el => el.style.opacity = "1");
+          document.querySelector(".intro3Image").style.animation = "moveInLeft 1s"
+          document.querySelector(".intro3Descr").style.animation = "moveInRight 1s"
+        }
+    }
+
+
+    scrollMain() {
+        document.addEventListener("scroll", (event) => {
+            this.lastKnownScrollPosition = window.scrollY;
+          
+            if (!this.ticking) {
+              window.requestAnimationFrame(() => {
+                this.setAnimations(this.lastKnownScrollPosition);
+                this.ticking = false;
+              });
+          
+              this.ticking = true;
+            }
+          })
     }
 }
 
