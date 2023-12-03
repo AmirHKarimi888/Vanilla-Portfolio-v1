84a965e45b9101d839ec09d8e4f0ebdc2f70006d
AmirHK888 <amirhkarimi888@gmail.com>
2023-12-03 14:41:10 +0330

    Vite top level fetch has been enabled

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-    build: {
-      target: 'esnext' //browsers can handle the latest ES features
-    }
-})
+  build: {
+    target: "es2022"
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    },
+  }
+});
